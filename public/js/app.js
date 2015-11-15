7d0fdd3888dba527a212f53769350b99da10b51c
Brandon Burning <bnburning3@gmail.com>
2015-11-14 22:48:38 -0700

    Fix URL

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@
 
   angular.module('ecreaylist').run(function () {
     var tag = document.createElement('script');
-    tag.src = "http://www.youtube.com/iframe_api";
+    tag.src = "//www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
   });
